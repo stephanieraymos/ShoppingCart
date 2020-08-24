68267ee676f9b9070310d3eae3488413b88355f3
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-08-23 22:13:18 -0700

    Cart total updating with removals

--- store.js
+++ store.js
@@ -1,9 +1,28 @@
 const removeCartItemButtons = document.getElementsByClassName('btn-danger')
 
-for(var i = 0; i < removeCartItemButtons.length; i++) {
+
+for (var i = 0; i < removeCartItemButtons.length; i++) {
     var button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event) {
+    button.addEventListener('click', function (event) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
     })
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
